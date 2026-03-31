4f9d1d63e71f7c789e5008356f9600b8f30b0fca
luisfelipe948 <luisdm.senac@gmail.com>
2026-03-31 09:48:02 -0300

    Delete Aula05 directory

--- Aula05/aula05.py
+++
@@ -1,8 +0,0 @@
-# contador = 1
-# while contador <= 5:
-#     print(contador)
-#     contador += 1 # mesma coisa que contador + 1
-
-for contador2 in range(20, -11, -1):
-       # if contador2 <= 20:
-            print(contador2)
